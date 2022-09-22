d3e1eb66e06c99380c51e378e94685e2023c6957
Ruurd Keizer <ruurdk@hotmail.com>
2022-09-22 16:01:59 +0200

    remove all spring exercises

--- exercises/exercise2/src/main/java/com/redis/lars/exercise2/MessageCrudRepository.java
+++
@@ -1,9 +0,0 @@
-package com.redis.lars.exercise2;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MessageCrudRepository extends CrudRepository<Message, String> {
-    
-}
--- exercises/exercise3/src/main/java/com/redis/lars/exercise3/MessageCrudRepository.java
+++
@@ -1,9 +0,0 @@
-package com.redis.lars.exercise3;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MessageCrudRepository extends CrudRepository<Message, String> {
-    
-}
